4e93ff14c3f0cf2e6532a0d9d1c3de678b34917e
579035 <oddneb@gmail.com>
2024-11-07 13:36:51 +0100

    Added tekst

--- src/main/java/no/hvl/dat100/oppgave2/Tekst.java
+++ src/main/java/no/hvl/dat100/oppgave2/Tekst.java
@@ -5,33 +5,40 @@ import no.hvl.dat100.oppgave1.*;
 
 public class Tekst extends Innlegg {
 
-	// TODO: objektvariable 
+	private String tekst;
 	
 	public Tekst () {
 		
 	}
 	
 	public Tekst(int id, String bruker, String dato, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
+		this.id = id;
+		this.bruker = bruker;
+		this.dato = dato;
+		this.tekst = tekst;
+		
 	}
 	
 	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
-		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
+		this.id = id;
+		this.bruker = bruker;
+		this.dato = dato;
+		this.likes = likes;
+		this.tekst = tekst;
 	}
 	
 	public String getTekst() {
-		throw new UnsupportedOperationException(TODO.method());
+		return this.tekst;
 
 	}
 
 	public void setTekst(String tekst) {
-		throw new UnsupportedOperationException(TODO.method());
+		this.tekst = tekst;
 	}
 
-	@Override
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		String string = String.valueOf(id)+"\n"+bruker+"\n"+dato+"\n"+String.valueOf(likes)+"\n"+tekst;
+		return string;
 	}
 	
 	// Metoden nedenfor er kun for valgfri oppgave 6
